0bd085e4fef233020f18e6569661c6d461276a4c
JFCresswell <jcresswell@hotmail.com>
2018-08-23 16:55:41 -0500

    rework

--- controllers/authenticationController.js
+++ controllers/authenticationController.js
@@ -1,10 +1,33 @@
 
+var tokens = [];
+
 var authenticationController = function(){
 
     var post = function(req, res){
         
         var payload = req.body;
-        //res.status(200).send({'result': 'posted json: '});
+        var token = '';
+
+        if (payload.authentication_request === "hash_add_from_password"){
+            token = simpleHash(payload.uid + payload.pwd);
+
+            tokens.push(token);
+            payload.token = token;
+        }
+        else if (payload.authentication_request === 'hash_add_from_clientid'){
+            token = simpleHash(payload.clientid + payload.clientname +
+                payload.servername);
+
+            tokens.push(token);
+            payload.token = token;
+
+        }
+        else if (payload.authentication_request === "verify_token"){
+            token = parseInt(payload.token_in);
+
+            payload.token_verified = tokens.indexOf(token) != -1 ? true : false;
+        }
+
         res.status(200).send(JSON.stringify(payload));
     }
 
@@ -17,6 +40,18 @@ var authenticationController = function(){
         post: post,
         get:get
     }
+}
+
+var simpleHash = function (str){
+  var hash = 0, i, chr;
+ 
+  for (i = 0; i < str.length; i++) {
+    chr   = str.charCodeAt(i);
+    hash  = ((hash << 5) - hash) + chr;
+    hash |= 0; // Convert to 32bit integer
+  }
+
+  return hash;
 }
 
 module.exports = authenticationController;
--- public/authentication.js
+++ public/authentication.js
@@ -1,13 +1,17 @@
 
-function OnAuthenticateClick(){
+const url='http://localhost:3000/api/authentication';
+
+function OnAuthenticatePasswordClick(){
     const http = new XMLHttpRequest();
-    const url='http://localhost:3000/api/authentication';
     http.open("POST", url, true);
     http.setRequestHeader("Content-Type", "application/json; charset=utf-8");
     
     var payload = {};
+    payload.authentication_request ="hash_add_from_password";
+
     payload.uid = document.getElementById('uid').value;
     payload.pwd = document.getElementById('pwd').value;
+
     var json = JSON.stringify(payload);
 
     http.send(json);
@@ -29,3 +33,49 @@ function OnAuthenticateClick(){
     //         JSON.stringify(myJson);
     // });
 }
+
+function OnAuthenticateClientIDClick(){
+    const http = new XMLHttpRequest();
+    http.open("POST", url, true);
+    http.setRequestHeader("Content-Type", "application/json; charset=utf-8");
+    
+    var payload = {};
+    payload.authentication_request ="hash_add_from_clientid";
+
+    payload.clientid = document.getElementById('clientid').value;
+    payload.clientname = document.getElementById('clientname').value;
+    payload.servername = document.getElementById('servername').value;
+    
+    var json = JSON.stringify(payload);
+
+    http.send(json);
+ 
+    http.onload = function () {
+        var result = document.getElementById("postResults");
+        result.innerText = http.responseText.length > 0 
+                            ? http.responseText : result.innerText;
+    }
+    
+}
+
+function OnVerifyTokenClick(){
+    const http = new XMLHttpRequest();
+    http.open("POST", url, true);
+    http.setRequestHeader("Content-Type", "application/json; charset=utf-8");
+    
+    var payload = {};
+    payload.authentication_request ="verify_token";
+
+    payload.token_in = document.getElementById('token').value;
+ 
+    var json = JSON.stringify(payload);
+
+    http.send(json);
+ 
+    http.onload = function () {
+        var result = document.getElementById("postResults");
+        result.innerText = http.responseText.length > 0 
+                            ? http.responseText : result.innerText;
+    }
+}
+
